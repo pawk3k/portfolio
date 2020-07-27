69c751eb296a78e28630c7937de427415f7a4250
kraven <ravliv.pavlobohdan@gmail.com>
2020-07-27 23:44:57 +0200

    Add margin skill fixed

--- src/Components/ThirdPage/Skill/Skill.js
+++ src/Components/ThirdPage/Skill/Skill.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const Skill = (props) =>{
     return (
-        <div className="row border border-dark border-10px" style={{marginBottom:"2vh"}}>
+        <div className="row border border-dark border-10px" style={{margin:"4vh 4vh 4vh 4vh"}}>
             <div className="col" style={{display:"flex",flexDirection:"column" ,justifyContent:"center",alignItems:"center"}}>
                 <div>
                     <i className={props.icon} style={{color: props.color ,height:"20px",width:"20px"}}/>
                 </div>
-                <div style={{color:"white",paddingLeft:"5vh"}} >
+                <div style={{color:"white" ,paddingLeft:"5vh"}}   >
                     {props.imgDesc}
                 </div>
             </div>
-            <div className="col">
+            <div className="col" style={{color:"white",padding:"2vh 2vh 2vh 2vh"}} >
                 {props.description}
             </div>
         </div>
--- src/Components/ThirdPage/ThirdPage.js
+++ src/Components/ThirdPage/ThirdPage.js
@@ -8,7 +8,7 @@ const ThirdPage =() =>{
             <div className="container">
                 <div style={{fontSize:"15vh",textDecoration:"underline",marginBottom:"5vh",color:"wheat"}}>Skills</div>
                 <Skill imgDesc={"React"} description={"I have build few projects with react ,react-redux and react router im able to solve different problems using this framework"} icon={"fab fa-react fa-5x"} color={"cyan"}/>
-                <Skill imgDesc={"HTML5"} description={"Kek1"} icon={"fab fa-html5 fa-5x"} color={"red"}/>
+                <Skill imgDesc={"HTML5"} description={"I use it to build websites"} icon={"fab fa-html5 fa-5x"} color={"red"}/>
                 <Skill imgDesc={"CSS3"} description={"I know how css works and able to make website more alive"} icon={"fab fa-css3-alt fa-5x"} color={"darkblue"}/>
                 <Skill description={"I know how to use sass to shortify css "} icon={"fab fa-sass fa-5x"} color={"deeppink"}/>
                 <Skill imgDesc={"Git"} description={"I use git in almost all of my projects i know about commits branches and all other basic staff"} icon={"fab fa-git-alt fa-5x"} color={"gold"}/>
