41afe181f3ecd90f2341f3ece7338c74343e7b7f
kraven <ravliv.pavlobohdan@gmail.com>
2020-08-06 18:19:20 +0200

    Add typescript

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SingleProjectTemplate from "./Components/FirstPage/SingleProjectTemplate";
 import SecondPage from "./Components/SecondPage/SecondPage";
 import ThirdPage from "./Components/ThirdPage/ThirdPage";
-import Contact from "./Components/ThirdPage/Contact/Contact";
-import Loader from './Components/SecondPage/Loader';
 function App() {
   return (
     <div className="App" style={{justifyContent:"center",display:"flex", width:"100vw",height:"100vh",flexWrap:"wrap"}}>
--- src/Components/ThirdPage/Skill/Skill.tsx
+++ src/Components/ThirdPage/Skill/Skill.tsx
@@ -1,18 +1,17 @@
 import React, { useRef,useEffect } from "react";
 import { TweenMax } from "gsap";
 import gsap from "gsap";
-// import gsap from "gsap";
-const Skill = (props) =>{
-    const skill_ref = useRef(null);
 
+interface Props {
+  imgDesc : string;
+  description : string;
+  icon : string;
+  color : string;
+}
+const Skill = ({imgDesc,description,icon,color}: Props) =>{
+    const skill_ref  = useRef<HTMLDivElement>(null);
     useEffect(() => {
-        // if(skill_ref){
-    // TweenMax.fromTo(
-    //   [skill_ref.current],
-    //   0.5,
-    //   { x: -180 },
-    //   { x: 0 }
-    // );
+        const header = skill_ref.current;
 
     gsap.from(skill_ref.current, {
         autoAlpha: 0,
@@ -21,23 +20,20 @@ const Skill = (props) =>{
           trigger: skill_ref.current,
           start: 'top bottom-=50px',
         },
-        // } 
     })
     },[]);
-        
-
     return (
         <div ref={skill_ref} className="row border border-dark border-10px" style={{margin:"4vh 4vh 4vh 4vh"}}>
             <div className="col" style={{display:"flex",flexDirection:"column" ,justifyContent:"center",alignItems:"center"}}>
                 <div>
-                    <i className={props.icon} style={{color: props.color ,height:"20px",width:"20px"}}/>
+                    <i className={icon} style={{color: color ,height:"20px",width:"20px"}}/>
                 </div>
                 <div style={{color:"white" ,paddingLeft:"5vh"}}   >
-                    {props.imgDesc}
+                    {imgDesc}
                 </div>
             </div>
             <div className="col" style={{color:"white",padding:"2vh 2vh 2vh 2vh"}} >
-                {props.description}
+                {description}
             </div>
         </div>
     )
