1b3e8fe248bdb0693a1f4e022cd7f571e6df098c
kraven <ravliv.pavlobohdan@gmail.com>
2020-08-06 11:28:52 +0200

    Add skill animation

--- src/Components/ThirdPage/Skill/Skill.js
+++ src/Components/ThirdPage/Skill/Skill.js
@@ -1,8 +1,23 @@
-import React from "react";
-
+import React, { useRef,useEffect } from "react";
+import { TweenMax } from "gsap";
+// import gsap from "gsap";
 const Skill = (props) =>{
+    const skill_ref = useRef(null);
+
+    useEffect(() => {
+        // if(skill_ref){
+    TweenMax.fromTo(
+      [skill_ref.current],
+      0.5,
+      { x: -180 },
+      { x: 0 }
+    );
+        // } 
+    },[]);
+        
+
     return (
-        <div className="row border border-dark border-10px" style={{margin:"4vh 4vh 4vh 4vh"}}>
+        <div ref={skill_ref} className="row border border-dark border-10px" style={{margin:"4vh 4vh 4vh 4vh"}}>
             <div className="col" style={{display:"flex",flexDirection:"column" ,justifyContent:"center",alignItems:"center"}}>
                 <div>
                     <i className={props.icon} style={{color: props.color ,height:"20px",width:"20px"}}/>
